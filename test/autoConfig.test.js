1c4efafce9f170ca91130f65aed9657e6b795abe
Otacilio Lacerda <otaciliolacerda@gmail.com>
2020-07-19 20:28:44 +0200

    fix: can use init with default params

--- test/autoConfig.test.js
+++ test/autoConfig.test.js
@@ -1,5 +1,11 @@
-describe('Test autoConfig', () => {
-  it('dummy test to setup jest', () => {
-    expect(true).toBeTruthy();
+const autoConfig = require('../lib/autoConfig');
+
+describe('test init', () => {
+  it('should throw error if profile is not defined', () => {
+    delete process.env.NODE_ENV;
+    expect(() => autoConfig.init()).toThrowError(
+      /No profile was given: set NODE_ENV or pass it as a parameter/
+    );
+    process.env.NODE_ENV = 'test';
   });
 });
